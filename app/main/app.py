5c7a060c58bd05dbcd38cad4a7137448c682909e
hriks <hriks@outlook.com>
2017-06-12 09:57:05 +0530

    fix private chat bug

--- app/main/app.py
+++ app/main/app.py
@@ -6,6 +6,7 @@ from flask_login import LoginManager,\
 from .. import app, User, db, Group, Group_user
 from forms import LoginForm, RegisterForm
 from flask import session, request
+from sqlalchemy import or_
 
 app.config['SECRET_KEY'] = 'Thisissupposedtobesecret!'
 Bootstrap(app)
@@ -75,29 +76,51 @@ def signup():
 @app.route('/dashboard', methods=['GET', 'POST'])
 @login_required
 def dashboard():
+    users = User.query.order_by(User.username)
+    groups = Group.query.order_by(Group.username)
+    return render_template(
+        'dashboard.html',
+        name=current_user.username,
+        users=users,
+        groups=groups,
+        session=session,
+    )
+
+
+@app.route('/private_chat', methods=['GET', 'POST'])
+@login_required
+def private_chat():
+    users = User.query.order_by(User.username)
+    groups = Group.query.order_by(Group.username)
     if request.method == 'POST':
         name = current_user.username
         session['name'] = str(name)
         room = str(request.form['submit'])
-        groups_users = Group_user.query.order_by(Group_user.username)
+        groups_users = Group_user.query.filter(or_(Group_user.user1==current_user.username, Group_user.user2==current_user.username)) # noqa
+        import pdb; pdb.set_trace()
         for user in groups_users:
-            if str(user.group) != room:
-                if str(user.group) != current_user.username:
-                    new_group = Group_user(
-                        username=session['name'],
-                        group=session['room']
-                    )
-                    db.session.add(new_group)
-                    db.session.commit()
-            continue
-        for user in groups_users:
-            if str(user.group) == str(current_user.username):
-                session['room'] = str(current_user.username)
-            elif str(user.group) == room:
-                session['room'] = room
-        print session['room']
-    users = User.query.order_by(User.username)
-    groups = Group.query.order_by(Group.username)
+            # session['room'] = str(user.id)
+            if (
+                str(user.user1) != str(
+                    current_user.username) and str(
+                    user.user2) != room) or (str(
+                    user.user1) != room and str(user.user2) != str(
+                    current_user.username)
+            ):
+                new_group = Group_user(
+                    user2=session['name'],
+                    user1=room
+                )
+                db.session.add(new_group)
+                db.session.commit()
+                session['room'] = str(user.id)
+#        for user in groups_users:
+#            if str(user.user1) == str(
+#                current_user.username) or str(
+#                    user.user1) == str(current_user.username):
+#                if str(user.user1) == room or str(user.user2) == room:
+#                    session['room'] = str(user.id)
+#        print session['room']
     return render_template(
         'dashboard.html',
         name=current_user.username,
