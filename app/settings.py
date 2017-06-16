c3993bb820c064a7e447c6a78d1546ff9eca0ee9
hriks <hriks@outlook.com>
2017-06-16 16:08:46 +0530

    Procfile added for runtime at heroku

--- app/settings.py
+++ app/settings.py
@@ -1,6 +1,7 @@
-import os
+# import os# 
 
-try:
-    CHAT_DATABASE_URI = os.environ['CHAT_DATABASE_URI']
-except Exception as e:
-    raise e
+# try:
+#     CHAT_DATABASE_URI = os.environ['CHAT_DATABASE_URI']
+# except Exception as e:
+#     raise e
+CHAT_DATABASE_URI='postgres://bqlxjind:OtFzlf08n1g8WLnLd7vPFoHGB3DFGkcd@pellefant.db.elephantsql.com:5432/bqlxjind'
